eb9ce3e5bdb0a46c8e5c46be356934231e7dedb3
Quarrrtzzz <sid1.97@mail.ru>
2022-12-29 19:15:26 +0300

    29dec

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        
+        
+        
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+      }
+            }
+        ]
+            
+
+        
+      });
+  });
